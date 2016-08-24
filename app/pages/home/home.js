33c545a6dcb05fb01f98cabe42f05970db34a2ff
Aaqib Hussain <silent_smah@hotmail.com>
2016-08-24 17:02:44 +0500

    Shows blank screen on map

---
+++ app/pages/home/home.js
@@ -0,0 +1,58 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var core_1 = require('@angular/core');
+var ionic_angular_1 = require('ionic-angular');
+var ionic_native_1 = require('ionic-native');
+var HomePage = (function () {
+    function HomePage(navCtrl, platform) {
+        var _this = this;
+        this.navCtrl = navCtrl;
+        this.platform = platform;
+        platform.ready().then(function () {
+            _this.loadMap();
+        });
+    }
+    HomePage.prototype.loadMap = function () {
+        var location = new ionic_native_1.GoogleMapsLatLng(-34.9290, 138.6010);
+        this.map = new ionic_native_1.GoogleMap('map', {
+            'backgroundColor': 'white',
+            'controls': {
+                'compass': true,
+                'myLocationButton': true,
+                'indoorPicker': true,
+                'zoom': true
+            },
+            'gestures': {
+                'scroll': true,
+                'tilt': true,
+                'rotate': true,
+                'zoom': true
+            },
+            'camera': {
+                'latLng': location,
+                'tilt': 30,
+                'zoom': 15,
+                'bearing': 50
+            }
+        });
+        this.map.on(ionic_native_1.GoogleMapsEvent.MAP_READY).subscribe(function () {
+            console.log('Map is ready!');
+        });
+    };
+    HomePage = __decorate([
+        core_1.Component({
+            templateUrl: 'build/pages/home/home.html'
+        }), 
+        __metadata('design:paramtypes', [ionic_angular_1.NavController, ionic_angular_1.Platform])
+    ], HomePage);
+    return HomePage;
+}());
+exports.HomePage = HomePage;
